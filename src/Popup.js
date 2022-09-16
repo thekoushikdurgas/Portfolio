63bd5a13904529fc55f43877103491e281e8f51b
thekoushikdurgas <thekoushikdurgas@gmail.com>
2022-09-16 05:43:25 +0530

    16/9

--- src/Popup.js
+++ src/Popup.js
@@ -13,7 +13,7 @@ export default function Popup() {
         <>
             <div className={`stick_block_layer ${popvisible ? '' : 'hidden'}`} id="stick_block_layer" style={{ zIndex: 9999 }}></div>
             <div className={`modalpop md:rounded-[1vw] rounded-[10px] bg-[#ffffff1a] shadow-[0_20px_50px_#00000026] border border-[#ffffff80] backdrop-blur-[5px] stick_popup ${popvisible ? "visible" : "hidden"}`} id="stickLayer">
-                <div className="stick_content gap-2">
+                <div className="stick_content gap-5">
                     <h2 className='top-[0] w-fit rounded-[0_0_10px_10px] md:rounded-[0_0_1vw_1vw] px-[10px] md:px-[1vw] text-[20px] md:text-[2vw] z-[200] m-[0_auto] bg-[#ffffff1a] shadow-[0_20px_50px_#00000026] border border-t-0 border-[#ffffff80] backdrop-blur-[5px]'>Copyright © Designed &amp; Developed by <a href={Copyright.link} target="_blank" rel="noopener noreferrer">{Copyright.name} </a> {Copyright.year}</h2>
                     <button className="close-btn close-modal stick_close" onClick={() => { getpopvisible(false) }}><i className="fas fa-times"></i></button>
                     <ul>
@@ -21,10 +21,10 @@ export default function Popup() {
                             <li className={`tkd${object.title} bg-[#ffffff1a] shadow-[0_20px_50px_#00000026] border border-[#ffffff80] backdrop-blur-[5px]`} key={i}><a href={object.link} target="_blank" rel="noopener noreferrer"><i className={object.icon}></i></a></li>
                         )}
                     </ul>
-                    <form action="/" method="post">
+                    <div>
                         <input id="subscribe-email" type="email" placeholder="Your email" name="email" className="email" />
-                        <input type="submit" name="subscribe" value="Subscribe" />
-                    </form>
+                        <button><i className="tkd8-email-mail-sent"></i></button>
+                    </div>
                     {/* <p onClick={()=>{getpopvisible(false)}} className='rounded-[10px_10px_0_0] md:rounded-[1vw_1vw_0_0] bg-[#ffffff1a] shadow-[0_20px_50px_#00000026] border border-[#ffffff80] backdrop-blur-[5px]'><button className="close close-modal stick_close">No thanks</button></p> */}
                 </div>
             </div>
